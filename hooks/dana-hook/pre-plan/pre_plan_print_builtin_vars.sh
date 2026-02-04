3dd23f56ac004b8e1ff5f9508cff861a05f3d356
danaroshchuk <b.roshchuk@scalr.com>
2026-02-04 17:15:09 +0200

    Update pre_plan_print_builtin_vars.sh

--- hooks/dana-hook/pre-plan/pre_plan_print_builtin_vars.sh
+++ hooks/dana-hook/pre-plan/pre_plan_print_builtin_vars.sh
@@ -1,32 +1,26 @@
-#!/usr/bin/env bash
-# Prints built-in workspace environment variables (Scalr / Terraform) to console.
-# Use as a hook or run manually to inspect run context.
+# Use as a hook or run manually. Uses POSIX sh so it works when Scalr runs with sh.
 
 set -e
 
-# Scalr built-in env vars (see https://docs.scalr.io/docs/run-environment)
-SCALR_BUILTIN_VARS=(
-  SCALR_RUN_ID
-  SCALR_TERRAFORM_OPERATION
-  SCALR_RUN_MODE
-  SCALR_RUN_IS_DESTROY
-  SCALR_WORKSPACE_NAME
-  SCALR_WORKSPACE_ID
-  SCALR_ENVIRONMENT_ID
-  SCALR_ENVIRONMENT_NAME
-  SCALR_RUN_VCS_BRANCH
-  SCALR_RUN_VCS_COMMIT
-  SCALR_HOSTNAME
-  SCALR_RUN_CONTENT_ROOT
-  SCALR_RUN_SOURCE
-  SCALR_TERRAFORM_EXIT_CODE
-)
-
 echo "=== Built-in workspace environment variables ==="
-for var in "${SCALR_BUILTIN_VARS[@]}"; do
-  if [[ -n "${!var+x}" ]]; then
-    printf '%s=%s\n' "$var" "${!var}"
-  else
-    printf '%s=(unset)\n' "$var"
-  fi
+
+# Scalr built-in env vars (see https://docs.scalr.io/docs/run-environment)
+for var in \
+  SCALR_RUN_ID \
+  SCALR_TERRAFORM_OPERATION \
+  SCALR_RUN_MODE \
+  SCALR_RUN_IS_DESTROY \
+  SCALR_WORKSPACE_NAME \
+  SCALR_WORKSPACE_ID \
+  SCALR_ENVIRONMENT_ID \
+  SCALR_ENVIRONMENT_NAME \
+  SCALR_RUN_VCS_BRANCH \
+  SCALR_RUN_VCS_COMMIT \
+  SCALR_HOSTNAME \
+  SCALR_RUN_CONTENT_ROOT \
+  SCALR_RUN_SOURCE \
+  SCALR_TERRAFORM_EXIT_CODE
+do
+  eval "val=\${$var-(unset)}"
+  printf '%s=%s\n' "$var" "$val"
 done
